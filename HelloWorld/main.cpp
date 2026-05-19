cf798465537ab08f09f5db50103e4cbb8fce36b4
Dominic Chen <cchendominic@gmail.com>
2026-05-19 14:06:47 -0400

    Testing with cin and cout

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
+#include <iostream>  // for std::cout and std::cin
 
-int main() {
-    std::cout << "Hello, world!";
-    std::cout << "C++ version: " << __cplusplus << std::endl;
+int main()
+{
+    std::cout << "Enter a number: "; // ask user for a number
+    int x{};
+    int y{};
+    int z{};
+
+    std::cin >> x >> y >> z; // read three numbers from the user
+
+    std::cout << "You entered " << x << " and " << y << " and " << z << "\n";
+
     return 0;
 }
