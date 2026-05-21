f2091585aa1275f8e074b6a5de5ec99f1b31dd42
Dominic Chen <cchendominic@gmail.com>
2026-05-20 22:58:36 -0400

    Write a function called doubleNumber()

---
+++ doubleNumber.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int doubleNumber(int x) {
+    return x * 2;
+}
+
+
+int main() {
+    std::cout << doubleNumber(2) << '\n';
+
+    return 0;
+}
--- multiplyBy2.cpp
+++ multiplyBy2.cpp
@@ -7,6 +7,7 @@ int main () {
     std::cin >> x;
 
     std::cout << "Double that number is: " << x * 2 << "\n";
+    std::cout << "Triple that number is: " << x * 3 << "\n";
 
     return 0;
 }
