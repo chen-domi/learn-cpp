59d87a63a227b56562be4b9394897355826d4fe4
Dominic Chen <cchendominic@gmail.com>
2026-05-23 21:25:48 -0400

    Add read and write functions to separate file

---
+++ io.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int readNumber()
+{
+    std::cout << "Enter a number to add: ";
+    int x{};
+    std::cin >> x;
+    return x;
+}
+
+void writeAnswer(int&x, int&y) 
+{
+    std::cout << x << " + " << y << " = " << x + y << '\n';
+}
--- main.cpp
+++ main.cpp
@@ -1,29 +1,7 @@
 #include <iostream>
 
-
-// that reads two separate integers from the user, 
-// adds them together, and then outputs the answer. 
-
-// void readNumber(int& x, int&y) 
-// {
-//     std::cout << "Enter two integers: ";
-//     std::cin >> x >> y;
-// }
-
-
-int readNumber()
-{
-    std::cout << "Enter a number to add: ";
-    int x{};
-    std::cin >> x;
-    return x;
-}
-
-void writeAnswer(int&x, int&y) 
-{
-    std::cout << x << " + " << y << " = " << x + y << '\n';
-}
-
+int readNumber();
+void writeAnswer(int&x, int&y);
 
 int main()
 { 
