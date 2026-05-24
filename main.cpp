d835af187e3bed73923b1640998aa0b75e2bd8ad
Dominic Chen <cchendominic@gmail.com>
2026-05-23 20:42:35 -0400

    Implement getValueFromUser
    This is to get the user value for inputs to a calculator.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int getUserInput() 
+{
+    std::cout << "Enter an integer: ";
+    int input{};
+    std::cin >> input;
+    return input;
+}
+
+int main() 
+{
+    int value{ getUserInput() };
+    std::cout << value << '\n';
+
+    return 0;
+}
