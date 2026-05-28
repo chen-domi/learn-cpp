fc6b581a3ea16de59dd1e126fe73e818e9cf5f5c
Dominic Chen <cchendominic@gmail.com>
2026-05-28 18:23:41 -0400

    Implement function to practice static_cast

--- main.cpp
+++ main.cpp
@@ -1,35 +1,12 @@
 #include <iostream>
 #include <vector>
 
-bool isPrime(int x) {
-    std::vector<int> primeNumbers = {2, 3, 5, 7};
-    return std::find(primeNumbers.begin(), primeNumbers.end(), x) != primeNumbers.end();
-}
-
-int getUserValue() {
-    int x{};
-
-    do {
-        std::cout << "Enter an integer 0-9: ";
-        std::cin >> x;
-
-        if (x < 0 || x > 9) {
-            std::cout << x << " is not a valid input,\n";
-        } 
-    } while (x < 0 || x > 9);
-
-    return x;
-}
-
 int main() {
-    int x{ getUserValue() };
-    
-    if (isPrime(x)) {
-        std::cout << x << " is a prime number \n";
-    }
-    else {
-        std::cout << x << " is not a prime number \n";
-    }
+    std::cout << "Enter a single character: ";
+    char ch{};
+    std::cin >> ch;
 
+    std::cout << "You entered '" << ch << "', which has ASCII code " << static_cast<int>(ch) << ".\n";
+    
     return 0;
 }
