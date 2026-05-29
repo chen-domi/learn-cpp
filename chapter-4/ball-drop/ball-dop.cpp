850369b0797d43cd417a27ddd7a9266c629862d2
Dominic Chen <cchendominic@gmail.com>
2026-05-29 01:34:23 -0400

    fix: add input validation and dynamic fall duration

--- chapter-4/ball-drop/ball-dop.cpp
+++ chapter-4/ball-drop/ball-dop.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
 // 1. Ask for height of the tower in meters --
 // 2. Program outputs the height of the ball above the ground after 0, 1, 2, 3, 4, 5 seconds
@@ -10,12 +11,17 @@
 // Use a function to calculate the height of the ball after x seconds.
 // Function calculates how far the ball has fallen after x seconds using: distance fallen = gravity_constant * x_seconds^2 / 2
 
-double getTowerHeight() 
+double getTowerHeight()
 {
-    std::cout << "Enter the height of the tower in meters: ";
     double towerHeight{};
-    std::cin >> towerHeight;
-    
+    do
+    {
+        std::cout << "Enter the height of the tower in meters: ";
+        std::cin >> towerHeight;
+        if (towerHeight <= 0)
+            std::cout << "Height must be positive.\n";
+    } while (towerHeight <= 0);
+
     return towerHeight;
 }
 
@@ -23,13 +29,14 @@ void calculateHeight(double height)
 {
     const double GRAVITY_CONSTANT{9.81};
     const double MIN_HEIGHT{0.0};
+    double timeToGround{std::sqrt((2 * height) / GRAVITY_CONSTANT)};
 
     std::cout << std::fixed << std::setprecision(1);
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < timeToGround + 1; i++)
     {
-        double distanceFallen {(GRAVITY_CONSTANT * (i * i)) / 2.0};
-        double currentHeight {height - distanceFallen};
+        double distanceFallen{(GRAVITY_CONSTANT * (static_cast<double>(i) * i)) / 2.0};
+        double currentHeight{height - distanceFallen};
 
         if (currentHeight > MIN_HEIGHT)
         {
@@ -44,7 +51,7 @@ void calculateHeight(double height)
 
 int main()
 {
-    double height {getTowerHeight()};
+    double height{getTowerHeight()};
     calculateHeight(height);
 
     return 0;
