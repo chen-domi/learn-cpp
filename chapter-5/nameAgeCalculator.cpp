90fe59523d439b5c4cb5807bf2f510250fa3d79b
Dominic Chen <cchendominic@gmail.com>
2026-05-31 14:32:03 -0400

    feat: add whitespace removal to string utilities

--- chapter-5/nameAgeCalculator.cpp
+++ chapter-5/nameAgeCalculator.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 int main() {
     std::cout << "Enter your full name: ";
     std::string name{};
     std::getline(std::cin >> std::ws, name);
+    name.erase(std::remove(name.begin(), name.end(), ' '), name.end());
+
 
     std::cout << "Enter your age: ";
     int age{};
     std::cin >> age;
-
+    
     int nameLength{ static_cast<int>(name.length())};
 
     std::cout << "Your age + length of name is: " << nameLength + age << '\n';
+    
     return 0;
 }
