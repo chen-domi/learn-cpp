4d85f395235914c371a8d272642575af9418ccc3
Dominic Chen <cchendominic@gmail.com>
2026-06-01 12:56:40 -0400

    refactor: make getName() and getAge()

--- chapter-5/whoIsOlder.cpp
+++ chapter-5/whoIsOlder.cpp
@@ -1,45 +1,57 @@
-// Write a program that asks for the name and age of two people, then prints which person is older.
-
-// Here is the sample output from one run of the program:
-
-// Enter the name of person #1: John Bacon
-// Enter the age of John Bacon: 37
-// Enter the name of person #2: David Jenkins
-// Enter the age of David Jenkins: 44
-// David Jenkins (age 44) is older than John Bacon (age 37).
 #include <iostream>
 #include <string>
 #include <string_view>
 
+std::string getFullName()
+{
+    std::string name{};
+    std::getline(std::cin >> std::ws, name);
+    return name;
+}
+
+int getAge(std::string_view person)
+{
+    std::cout << "Enter the age of " << person << ": ";
+    int age{};
+    std::cin >> age;
+    return age;
+}
+
+void calculateWhoIsOlder() {
+
+}
+
 int main()
 {
     std::cout << "Enter the name of person #1: ";
-    std::string name1{};
-    std::getline(std::cin >> std::ws, name1);
-
-    std::cout << "Enter the age of " << name1 << ": ";
-    int age1{};
-    std::cin >> age1;
+    std::string p1{getFullName()};
+    int age1{getAge(p1)};
 
     std::cout << "Enter the name of person #2: ";
-    std::string name2{};
-    std::getline(std::cin >> std::ws, name2);
+    std::string p2{getFullName()};
+    int age2{getAge(p2)};
 
-    std::cout << "Enter the age of " << name2 << ": ";
-    int age2{};
-    std::cin >> age2;
+    // std::cout << p1 << " and " << p2 << '\n';
+    // std::cout << age1 << "and" << age2 << '\n';
 
-    // std::cout << name1 << " is " << age1 << '\n';
-    // std::cout << name2 << " is " << age2 << '\n';
-   
-   
-    if (age1 > age2)
-    {
-        std::cout << name1 << " (age " << age1 << ')' << " is older than " << name2 << " (age " << age2 << ").\n";
-    }
-    else
-    {
-        std::cout << name2 << " (age " << age2 << ')' << " is older than " << name1 << " (age " << age1 << ").\n";
-    }
-    return 0;
+    std::cout << p1 << " is " << age1 << '\n';
+    std::cout << p2 << " is " << age2 << '\n';
+
+    // std::cout << "Enter the age of " << name1 << ": ";
+    // int age1{};
+    // std::cin >> age1;
+
+    // std::cout << "Enter the age of " << name2 << ": ";
+    // int age2{};
+    // std::cin >> age2;
+
+    // if (age1 > age2)
+    // {
+    //     std::cout << name1 << " (age " << age1 << ')' << " is older than " << name2 << " (age " << age2 << ").\n";
+    // }
+    // else
+    // {
+    //     std::cout << name2 << " (age " << age2 << ')' << " is older than " << name1 << " (age " << age1 << ").\n";
+    // }
+    // return 0;
 }
