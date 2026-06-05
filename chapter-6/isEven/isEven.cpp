6d65f15fde5133afb0a8ef513ff13dccffeacc2d
Dominic Chen <cchendominic@gmail.com>
2026-06-05 16:57:02 -0400

    feat: check if an int is even or odd

---
+++ chapter-6/isEven/isEven.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+// 1. Write a program that asks the user to input an integer
+// 2. Tells the user whether the number is even or odd
+// 3. Write a contexpr function called isEven() that returns true or false
+// Use the remained operator to test whether the integer parameter is even
+// Make sure isEven() works with both positive and negative numbers
+
+constexpr bool isEven(int x)
+{
+    return x % 2 == 0;
+}
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int x{};
+    std::cin >> x;
+
+    if (isEven(x))
+    {
+        std::cout << x << " is an even number" << '\n';
+    } else 
+    {
+        std::cout << x << " is an odd number" << '\n';
+    }
+    
+    return 0;
+}
