9f8bcbf720dfcaaedad4177948eb048af514c200
Dominic Chen <cchendominic@gmail.com>
2026-06-06 02:47:48 -0400

    feat: program to determine apple quantity

---
+++ chapter-6/question-1/q1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+// Write the function getQuantityPhrase() here
+std::string getQuantityPhrase(int quantity) 
+{
+    if (quantity < 0)
+        return "negative";
+    if (quantity == 0)
+        return "no";
+    if (quantity == 1)
+        return "a single";
+    if (quantity ==2)
+        return "a couple of";
+    if (quantity == 3)
+        return "a few";
+    return "many";
+
+}
+// Write the function getApplesPluralized() here
+std::string getApplesPluralized(int quantity)
+{
+    return (quantity == 1) ? "apple" : "apples";
+}
+
+int main()
+{
+    constexpr int maryApples { 3 };
+    std::cout << "Mary has " << getQuantityPhrase(maryApples) << ' ' << getApplesPluralized(maryApples) << ".\n";
+
+    std::cout << "How many apples do you have? ";
+    int numApples{};
+    std::cin >> numApples;
+
+    std::cout << "You have " << getQuantityPhrase(numApples) << ' ' << getApplesPluralized(numApples) << ".\n";
+
+    return 0;
+}
