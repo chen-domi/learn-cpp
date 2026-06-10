f45bdfe190b356c452c81b136f6f88d6c00a3ea2
Dominic Chen <cchendominic@gmail.com>
2026-06-10 13:54:00 -0400

    feat: set article as viewed with bit mask

---
+++ chapter-o/bit-manipulation-with-bitwise-operators-and-bit-masks/q1.cpp
@@ -0,0 +1,21 @@
+#include <bitset>
+#include <cstdint>
+#include <iostream>
+
+int main()
+{
+    [[maybe_unused]] constexpr std::uint8_t option_viewed{ 0x01 };
+    [[maybe_unused]] constexpr std::uint8_t option_edited{ 0x02 };
+    [[maybe_unused]] constexpr std::uint8_t option_favorited{ 0x04 };
+    [[maybe_unused]] constexpr std::uint8_t option_shared{ 0x08 };
+    [[maybe_unused]] constexpr std::uint8_t option_deleted{ 0x10 };
+
+    std::uint8_t myArticleFlags{ option_favorited };
+
+    myArticleFlags |= option_viewed; // set the article as viewed
+    // check if the article was deleted
+
+    std::cout << std::bitset<8>{ myArticleFlags } << '\n';
+
+    return 0;
+}
